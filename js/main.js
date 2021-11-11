9f3b975e4c9870720295db2558934ceb01c5b6cb
Joseph Asante <freecoding1@gmail.com>
2021-11-11 18:36:37 +0000

    Slider with Animating text

--- js/main.js
+++ js/main.js
@@ -7,4 +7,11 @@
         perturbance: 0.01,
         
       });
- })
+
+      $(".text").typed({
+          strings:["<strong>i love </strong> <strong class='primary'>coding.</strong>", 
+          "<strong>and to </strong><strong class='primary'>share !!.</strong>"],
+          typespeed: 0,
+          loop: true
+      });
+ });
